8c8ee821eae2f256af395a81b9844216ff9a06bf
nooob248 <82915834+nooob248@users.noreply.github.com>
2021-08-18 12:51:30 -0400

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,12 @@
+class Bob {
+    constructor(x,y,width,height){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+
+        ellipse(56, 46, 55, 55);
+    }
+}
